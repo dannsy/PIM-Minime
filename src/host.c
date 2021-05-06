e09d2f15b7d7245fa5c641abdc5664a8efa267f0
Daniel Ng <danielnsyu@protonmail.com>
2021-05-05 18:50:33 -0700

    feat: fixed small bug with passing inputs to DPU, added time recording for copy in and copy out, updated python script

--- src/host.c
+++ src/host.c
@@ -89,7 +89,7 @@ void rand_init(dpu_input_t *dpu_input)
         input_buffer[index] = rand_array[i].i;
         index = input_buffer[index];
 
-        if (i % dpu_input->tasklet_buffer_size == 0)
+        if (i % dpu_input->tasklet_buffer_size == 0 && j < NR_TASKLETS)
         {
             dpu_input->tasklet_start_index[j] = index;
             j++;
@@ -100,8 +100,6 @@ void rand_init(dpu_input_t *dpu_input)
     free(rand_array);
 }
 
-// TODO: Calculate time spent doing benchmarking
-// TODO: Let benchmark run until time limit reached instead of only doing one loop
 void prepare_dpu()
 {
     struct dpu_set_t dpu_set, dpu;
@@ -129,12 +127,18 @@ void prepare_dpu()
         rand_init(&dpu_input);
     }
 
+    TIME_NOW(&start);
     DPU_ASSERT(dpu_copy_to(dpu_set, "buffer", 0, input_buffer, per_dpu_memory_to_alloc));
     DPU_ASSERT(dpu_copy_to(dpu_set, "input", 0, &dpu_input, sizeof(dpu_input_t)));
+    TIME_NOW(&end);
+
+    float copy_in_time = TIME_DIFFERENCE(start, end);
 
     TIME_NOW(&start);
     DPU_ASSERT(dpu_launch(dpu_set, DPU_SYNCHRONOUS));
     TIME_NOW(&end);
+
+    float benchmark_time = TIME_DIFFERENCE(start, end);
 
     // DPU_FOREACH(dpu_set, dpu)
     // {
@@ -143,15 +147,21 @@ void prepare_dpu()
 
     dpu_output_t results[NR_DPUS];
     uint32_t each_dpu;
-    uint64_t bytes_read = 0, cycles = 0;
+
+    TIME_NOW(&start);
     DPU_FOREACH(dpu_set, dpu, each_dpu)
     {
         DPU_ASSERT(dpu_prepare_xfer(dpu, &results[each_dpu]));
     }
     DPU_ASSERT(dpu_push_xfer(dpu_set, DPU_XFER_FROM_DPU, "results", 0, sizeof(dpu_output_t), DPU_XFER_DEFAULT));
+    TIME_NOW(&end);
 
+    float copy_out_time = TIME_DIFFERENCE(start, end);
+
+    uint64_t total_bytes_read = 0;
     DPU_FOREACH(dpu_set, dpu, each_dpu)
     {
+        uint64_t bytes_read = 0, cycles = 0;
         // retrieve tasklet results
         for (unsigned int each_tasklet = 0; each_tasklet < NR_TASKLETS; each_tasklet++)
         {
@@ -161,15 +171,25 @@ void prepare_dpu()
             cycles += result->cycles;
         }
 
-        cycles /= NR_TASKLETS;
+        // cycles /= NR_TASKLETS;
 
-        float mbytes_read = bytes_read / 1024. / 1024.;
-        float time_diff = TIME_DIFFERENCE(start, end);
-        printf("\tTotal bytes read: %lu bytes (%.2f MB)\n", bytes_read, mbytes_read);
-        printf("\tAverage cycles per DPU: %lu cycles\n", cycles);
-        printf("\tTime taken to run benchmark: %f seconds\n", time_diff);
-        printf("\tThroughput: %.2f MB/s\n", mbytes_read / time_diff);
+        // float mbytes_read = bytes_read / 1024. / 1024.;
+        // printf("\nBytes read: %lu bytes (%.2f MB)\n", bytes_read, mbytes_read);
+        // printf("Average cycles per DPU: %lu cycles\n", cycles);
+        // printf("Time taken to run benchmark: %f seconds\n", time_diff);
+        // printf("Throughput: %.2f MB/s\n", mbytes_read / time_diff);
+
+        total_bytes_read += bytes_read;
     }
+
+    float total_mbytes_read = total_bytes_read / 1024. / 1024.;
+    printf("Bench results\n");
+    printf("\t* Copy in took: %fs\n", copy_in_time);
+    printf("\t* Benchmarking took: %fs\n", benchmark_time);
+    printf("\t* Copy out took: %fs\n", copy_out_time);
+
+    printf("\t* Total bytes read: %lu bytes (%.2f MB)\n", total_bytes_read, total_mbytes_read);
+    printf("\t* Overall throughput: %.2f MB/s\n", total_mbytes_read / benchmark_time);
 
     DPU_ASSERT(dpu_free(dpu_set));
 }
@@ -201,7 +221,6 @@ static void usage(char *app_name)
         printf("\t\t%d - %s\n", i, plugins[i].name);
     }
 
-    fprintf(stderr, "\t-d: use DPU\n");
     fprintf(stderr, "\t-g: memory size to benchmark per DPU (Maximum 32MB)\n");
     fprintf(stderr, "\t-T: manually specify the benchmark duration (in seconds)\n");
     fprintf(stderr, "\t-h: display usage\n");
@@ -209,18 +228,12 @@ static void usage(char *app_name)
 
 int main(int argc, char **argv)
 {
-    bool use_dpu = false;
-
     int opt;
-    char const options[] = "hdt:g:T:";
+    char const options[] = "ht:g:T:";
     while ((opt = getopt(argc, argv, options)) != -1)
     {
         switch (opt)
         {
-        case 'd':
-            use_dpu = true;
-            break;
-
         case 't':
             chosen_plugin = atoi(optarg);
             if (chosen_plugin < 0 || chosen_plugin >= nb_plugins)
@@ -262,20 +275,14 @@ int main(int argc, char **argv)
     buffer_size = per_dpu_memory_to_alloc / sizeof(uint32_t);
 
     printf("Bench parameters\n");
-    printf("\t* Use DPU: %d\n", use_dpu);
+    printf("\t* Num DPUs: %d\n", NR_DPUS);
+    printf("\t* Num tasklets: %d\n", NR_TASKLETS);
     printf("\t* Chosen benchmark: %s\n", plugins[chosen_plugin].name);
     printf("\t* Memory size to bench per DPU: %u bytes\n", per_dpu_memory_to_alloc);
     printf("\t* Buffer size: %u\n", buffer_size);
     printf("\t* Benchmark time: %lus\n", (unsigned long)bench_time);
 
-    if (use_dpu)
-    {
-        prepare_dpu();
-    }
-    else
-    {
-        // TODO: add CPU only code here
-    }
+    prepare_dpu();
 
     return EXIT_SUCCESS;
 }
--- tools/run_dpu.py
+++ tools/run_dpu.py
@@ -6,24 +6,34 @@ import os
 import subprocess
 from datetime import datetime
 
-ITERATIONS = 10
-DEBUG = 1
+DEBUG = 0
 
 
-def make(num_dpus=1, num_tasklets=1):
+def make(num_dpus=1, num_tasklets=1, log=False):
+    # Calling make clean and make with the right number of DPUs and tasklets
     command = [
         f"make clean && make NR_DPUS={num_dpus} NR_TASKLETS={num_tasklets}",
     ]
     if DEBUG:
         print(f"Command being executed: {command}")
 
-    print()
+    subprocess.run(command, shell=True, capture_output=not log)
+
+
+def execute(mode, total_mem="32M", bench_time=10):
+    command = [f"./host -t {mode} -g {total_mem} -T {bench_time}"]
     subprocess.run(command, shell=True)
 
 
-def benchmark(mode, total_mem="32M", bench_time=10):
-    command = [f"./host -d -t {mode} -g {total_mem} -T {bench_time}"]
-    subprocess.run(command, shell=True)
+def benchmark(num_dpus, mode):
+    # The number of tasklets to benchmark with
+    tasklets_list = [1, 2, 4, 8, 12, 16, 24]
+
+    stars = "*" * 28
+    for num in tasklets_list:
+        print(f"\n{stars} Benchmark with {num_dpus} DPUs and {num} tasklets {stars}\n")
+        make(num_dpus, num)
+        execute(mode, bench_time=0)
 
 
 if __name__ == "__main__":
@@ -35,17 +45,13 @@ if __name__ == "__main__":
 
     # Getting user input
     num_dpus = int(input("Number of DPUs: "))
-    num_tasklets = int(input("Number of tasklets: "))
     mode = input("Choose mode (0: sequential read, 1: random read): ")
-    bench_time = int(input("Benchmark time: "))
-
-    # Calling make clean and make with the right number of DPUs and tasklets
-    make(num_dpus, num_tasklets)
+    # bench_time = int(input("Benchmark time: "))
 
     start_stars = "*" * 30
     end_stars = "*" * 32
     print(f"\n{start_stars} Starting benchmark for Minime {start_stars}\n")
 
-    benchmark(mode, bench_time=bench_time)
+    benchmark(num_dpus, mode)
 
     print(f"\n{end_stars} Done benchmark for Minime {end_stars}")
