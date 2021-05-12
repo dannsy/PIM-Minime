cf133b204c8d6d028fbbeeb1beb5705cef744000
Daniel Ng <danielnsyu@protonmail.com>
2021-05-11 19:32:56 -0700

    feat: cleaned up wram_transfer benchmark and compiled results

--- src/dpu/wram_transfer.c
+++ src/dpu/wram_transfer.c
@@ -29,43 +29,24 @@ int main()
     {
         perfcounter_config(COUNT_CYCLES, true);
     }
-    uint64_t average_cycles = 0;
+    uint64_t transfer_cycles = 0;
     uint64_t start_cycle, end_cycle;
 
-    for (int i = 0; i < 5; i++) {
-        uint32_t buffer_i = input.tasklet_start_index[tasklet_id];
-        // uint32_t end_i = buffer_i + buffer_size_per_tasklet;
-        uint32_t end_i = buffer_i + BLOCK_SIZE * 1024;
+    uint32_t buffer_i = input.tasklet_start_index[tasklet_id];
+    // uint32_t end_i = buffer_i + BLOCK_SIZE * 1024;
 
-        for (; buffer_i < end_i; buffer_i += BLOCK_SIZE)
-        {
-            start_cycle = perfcounter_get();
-            // read MRAM buffer to WRAM cache
-            mram_read(&buffer[buffer_i], cache, BLOCK_SIZE * sizeof(uint32_t));
-            end_cycle = perfcounter_get();
-            average_cycles += end_cycle - start_cycle;
+    __mram_ptr void *buffer_addr = (__mram_ptr void *)&buffer;
+    uint32_t transfer_size = BLOCK_SIZE * sizeof(uint32_t);
+    start_cycle = perfcounter_get();
+    // read MRAM buffer to WRAM cache
+    mram_read(buffer_addr, cache, transfer_size);
+    end_cycle = perfcounter_get();
+    transfer_cycles = end_cycle - start_cycle;
 
-            // unroll loop to have greater ratio of load instructions to branch instructions
-            // for (uint32_t block_i = 0; block_i < BLOCK_SIZE; block_i += 8)
-            // {
-            //     sum += cache[block_i + 0];
-            //     sum += cache[block_i + 1];
-            //     sum += cache[block_i + 2];
-            //     sum += cache[block_i + 3];
-            //     sum += cache[block_i + 4];
-            //     sum += cache[block_i + 5];
-            //     sum += cache[block_i + 6];
-            //     sum += cache[block_i + 7];
-            // }
-        }
-
-        nb_iterations++;
-    }
-
-    printf("Cycles: %lu\n", average_cycles / (5 * 1024));
+    printf("Transfer cycles: %lu, transfer size: %lu bytes\n", transfer_cycles, transfer_size);
     printf("Sum value: %u\n", sum);
 
     result->cycles = perfcounter_get();
-    result->bytes_read = nb_iterations * buffer_size_per_tasklet * sizeof(uint32_t);
+    result->bytes_read = buffer_size_per_tasklet * sizeof(uint32_t);
     return 0;
 }
